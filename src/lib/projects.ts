5bd03bb641f3bff0df6c946e2f3ddf82e82296d0
Cristian Alejandro Barajas Bolivar <cristianbarajasimasc@gmail.com>
2024-04-20 21:03:21 -0400

    feat: Create a first componets

---
+++ src/lib/projects.ts
@@ -0,0 +1,17 @@
+export const PROJECTS = [
+    {
+        name: "Project 1",
+        description: "This is a project",
+        url: "https://www.google.com"
+    },
+    {
+        name: "Project 2",
+        description: "This is a project",
+        url: "https://www.google.com"
+    },
+    {
+        name: "Project 3",
+        description: "This is a project",
+        url: "https://www.google.com"
+    }
+]
